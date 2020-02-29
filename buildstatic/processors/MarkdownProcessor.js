7b7d074c6ddfc79aa1039bf076445bf7475bdb93
Damien Dart <damiendart@pobox.com>
2020-02-29 13:14:55 +0000

    Add Markdown file processor to "buildstatic".

---
+++ buildstatic/processors/MarkdownProcessor.js
@@ -0,0 +1,41 @@
+// Copyright (C) 2020 Damien Dart, <damiendart@pobox.com>.
+// This file is distributed under the MIT licence. For more information,
+// please refer to the accompanying "LICENCE" file.
+
+/* eslint-env node */
+
+const grayMatter = require('gray-matter');
+const marked = require('marked');
+const TwigProcessor = require('./TwigProcessor');
+
+class MarkdownProcessor {
+  static get INPUT_EXTENSION() {
+    return '.md';
+  }
+
+  static get OUTPUT_EXTENSION() {
+    return '.html';
+  }
+
+  static process(content, context) {
+    const parsed = grayMatter(content, { delimiters: ['<!--', '-->'] });
+    const renderedMarkdown = marked(
+      parsed.content,
+      { headerIds: false, smartypants: true },
+    );
+
+    if ('twigTemplate' in parsed.data) {
+      const twigContext = context;
+
+      twigContext.inputFile.name = parsed.data.twigTemplate;
+      twigContext.renderedMarkdown = renderedMarkdown;
+      twigContext.frontmatter = parsed.data;
+
+      return TwigProcessor.process('', twigContext);
+    }
+
+    return Promise.resolve(renderedMarkdown);
+  }
+}
+
+module.exports = MarkdownProcessor;
