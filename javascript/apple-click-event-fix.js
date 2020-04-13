12f66dc90275838ac60cc75f808fc10d6f4eaf40
Damien Dart <damiendart@pobox.com>
2020-04-13 10:26:00 +0100

    Update "apple-click-event-fix.js".

--- javascript/apple-click-event-fix.js
+++ javascript/apple-click-event-fix.js
@@ -5,29 +5,26 @@
 // Account for iOS and iPadOS WebKit's busted handling of mouse events.
 // See <https://www.quirksmode.org/blog/archives/2014/02/mouse_event_bub.html>
 // for more information. This fix is based on one used by Bootstrap.
-class AppleClickEventFix {
-  static applyFix() {
-    if ('ontouchstart' in document.documentElement) {
-      // Internet Explorer 11 doesn't implement `NodeList.forEach()`,
-      // hence the `Array.prototype.forEach()` kludge. See
-      // <https://developer.mozilla.org/en-US/docs/Web/API/NodeList#Example>
-      // for more information.
-      Array.prototype.forEach.call(document.body.children, (element) => {
-        element.addEventListener('mouseover', this.noop);
-      });
-    }
-  }
-
-  // eslint-disable-next-line class-methods-use-this
-  static noop() {
-    return () => {};
-  }
-
-  static removeFix() {
+function applyFix() {
+  if ('ontouchstart' in document.documentElement) {
+    // Internet Explorer 11 doesn't implement `NodeList.forEach()`,
+    // hence the `Array.prototype.forEach()` kludge. See
+    // <https://developer.mozilla.org/en-US/docs/Web/API/NodeList#Example>
+    // for more information.
     Array.prototype.forEach.call(document.body.children, (element) => {
-      element.removeEventListener('mouseover', this.noop);
+      element.addEventListener('mouseover', this.noop);
     });
   }
 }
 
-module.exports = AppleClickEventFix;
+function noop() {
+  return () => {};
+}
+
+function removeFix() {
+  Array.prototype.forEach.call(document.body.children, (element) => {
+    element.removeEventListener('mouseover', this.noop);
+  });
+}
+
+module.exports = { applyFix, noop, removeFix };
