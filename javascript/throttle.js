a1d198d2823bac7b605f780ad25031cb43506741
Damien Dart <damiendart@pobox.com>
2020-05-02 10:57:18 +0100

    Add a JavaScript throttling doohickey.

---
+++ javascript/throttle.js
@@ -0,0 +1,28 @@
+// Copyright (C) 2020 Damien Dart, <damiendart@pobox.com>.
+// This file is distributed under the MIT licence. For more information,
+// please refer to the accompanying "LICENCE" file.
+
+// The following is based on
+// <https://programmingwithmosh.com/javascript/javascript-throttle-and-debounce-patterns/>.
+function throttle(callback, interval = 0) {
+  let throttled = true;
+
+  // eslint-disable-next-line func-names
+  return function (...args) {
+    if (!throttled) {
+      return;
+    }
+
+    throttled = false;
+    callback.apply(this, args);
+    if (interval > 0) {
+      // eslint-disable-next-line no-return-assign
+      setTimeout(() => throttled = true, interval);
+    } else {
+      // eslint-disable-next-line no-return-assign
+      window.requestAnimationFrame(() => throttled = true);
+    }
+  };
+}
+
+module.exports = throttle;
